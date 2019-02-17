70dd3f08afaeb230f67a6c66f716828255ea797f
Henry Thach <thachhao@gmail.com>
2019-02-17 08:36:39 -0500

    Add entry scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_123457) do
+ActiveRecord::Schema.define(version: 2019_02_17_134422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,4 +49,5 @@ ActiveRecord::Schema.define(version: 2019_02_17_123457) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "entries", "users"
 end
